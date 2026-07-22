2a6e577bb2026d69ce746788024fc99def5e0b32
venetsanoum <marinavenetsanou7@gmail.com>
2026-07-22 14:22:28 +0300

    bug

--- task-management/src/main/java/com/example/task_management/security/JWTFilter.java
+++ task-management/src/main/java/com/example/task_management/security/JWTFilter.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
@@ -15,6 +16,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class JWTFilter extends OncePerRequestFilter{ // for every request use this filter once 
     @Autowired
     private JWTService jwtService;
--- task-management/src/main/java/com/example/task_management/security/SecurityConfig.java
+++ task-management/src/main/java/com/example/task_management/security/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 /* The security pipeline */
 @Configuration
 @EnableWebSecurity
